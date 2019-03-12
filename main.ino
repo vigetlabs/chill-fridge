0948d6001f909982b8f19a921d7226b7e9f653cb
Ben Tinsley <ben.tinsley@viget.com>
2019-03-12 09:37:45 -0600

    uses ws2821 library and sets conditionals to open/close door patterns

--- main.ino
+++ main.ino
@@ -1,91 +1,57 @@
-#include "lib/neopixel.h"
+// This #include statement was automatically added by the Particle IDE.
+#include <WS2812FX.h>
+
+#include "neopixel.h"
 
 // SYSTEM_MODE(MANUAL);
 
-#define PIXEL_PIN D2
-#define PIXEL_COUNT 126
+#define LED_PIN D2
+#define LED_COUNT 126
 #define PIXEL_TYPE WS2812B
 
-Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+#define DOOR_PIN D6
+#define CURRENT_PIN D4
+#define ANALOG_THRESHOLD 512
 
-void setup()
-{
-  // Initialize all pixels to 'off'
-  strip.begin();
-  strip.show();
 
-  // Set brightness (0 - 255)
-  strip.setBrightness(255);
+
+// https://github.com/kitesurfer1404/WS2812FX
+// multiplies led count by 4 to make the rainbow effect show less color at once
+WS2812FX ws2812fx = WS2812FX(LED_COUNT*4, LED_PIN, WS2812B);
+
+bool shouldPatternReset = TRUE;
+
+// segmented pixel values for side specific patterns, if desired
+int firstPixel = 0;
+int topStartPixel = 42;
+int leftStartPixel = 63;
+int bottomStartPixel = 105;
+int lastPixel = 126;
+
+void setup() {
+    ws2812fx.init();
+    ws2812fx.setBrightness(180);
+    resetPattern();
+    ws2812fx.start();
 }
 
 void loop() {
-  rainbow(20);
-}
 
-void colorTheSides() {
-  for (size_t i = 0; i < 42; i++) {
-    strip.setPixelColor(i, strip.Color(255,0,0));
-  }
-  for (size_t i = 42; i < 63; i++) {
-    strip.setPixelColor(i, strip.Color(0,255,255));
-  }
-  for (size_t i = 63; i < 105; i++) {
-    strip.setPixelColor(i, strip.Color(255,0,255));
-  }
-  for (size_t i = 105; i < 126; i++) {
-    strip.setPixelColor(i, strip.Color(255,255,255));
-  }
-  strip.show();
-}
+    ws2812fx.service();
 
-void solidColors() {
-  for (size_t i = 0; i < 126; i++) {
-    strip.setPixelColor(i, strip.Color(255,0,0));
-  }
-  strip.show();
-  delay(2000);
-
-  for (size_t i = 0; i < 126; i++) {
-    strip.setPixelColor(i, strip.Color(0,255,0));
-  }
-  strip.show();
-  delay(2000);
-
-  for (size_t i = 0; i < 126; i++) {
-    strip.setPixelColor(i, strip.Color(0,0,255));
-  }
-  strip.show();
-  delay(2000);
-
-  for (size_t i = 0; i < 126; i++) {
-    strip.setPixelColor(i, strip.Color(255,255,255));
-  }
-  strip.show();
-  delay(2000);
-}
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+    if(digitalRead(DOOR_PIN) == LOW) {
+        ws2812fx.setColor(0xFFFFFF);
+        ws2812fx.setMode(FX_MODE_STATIC);
+        shouldPatternReset = TRUE;
+    } else {
+        resetPattern();
     }
-    strip.show();
-    delay(wait);
-  }
 }
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
+void resetPattern() {
+    if(shouldPatternReset == TRUE) {
+        ws2812fx.setSpeed(15000);
+        ws2812fx.setMode(FX_MODE_RAINBOW_CYCLE);
+        shouldPatternReset = FALSE;
+    }
 }
